3a17eb288d7396111ea96626128c9accee6eb926
nakano16180 <nakano16180@gmail.com>
2021-06-11 09:06:12 +0000

    Feature/preset react

--- routes/web.py
+++ routes/web.py
@@ -4,11 +4,7 @@ from masonite.routes import Get, Post
 
 ROUTES = [
     Get("/", "WelcomeController@show").name("welcome"),
-]
-# masonite-inertia demo routes
-ROUTES += [
-    Get("/inertia", "InertiaDemoController@show"),
-    Get("/inertia-hello", "InertiaDemoController@hello"),
+    Get("/react", "ReactDemoController@show"),
 ]
 
 from masonite.auth import Auth 
